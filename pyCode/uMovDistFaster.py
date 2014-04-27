58d6a4a9a2752b43fee20ad66ca14ed50ce5f4ee
jztein <jiayuaw@gmail.com>
2014-04-26 23:37:34 -0700

    sped up 4Ks to 470s with npzfiles

--- pyCode/uMovDistFaster.py
+++ pyCode/uMovDistFaster.py
@@ -9,6 +9,7 @@ from time import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c')
+parser.add_argument('--breakdown', action="store_true")
 
 args = parser.parse_args()
 csvFile = open(args.c, 'r')
@@ -17,6 +18,12 @@ numLines = len(allLinesCsv)
 header = allLinesCsv[0]
 
 avgFile = open('avgs.txt', 'w')
+
+def distributionOfMoviesUsers(pids, uids):
+    print "plotting..."
+
+    plt.scatter(pids, uids, alpha=0.3)
+    plt.savefig("scatterFastest.png")
 
 def shortenFile():
     smallFile = open('aMsmall_Ascii.csv', 'w')
@@ -29,19 +36,25 @@ def shortenFile():
     smallFile.close()
 
 def breakdownLines(allLines):
-    newAllLines = []
-    for i in xrange(1, numLines):
-        curLine = allLines[i]
-        newAllLines.append(curLine.split(','))
-    return newAllLines
+    pidArr = np.zeros(numLines-1)
+    uidArr = np.zeros(numLines-1)
 
-def groupByMovies(allLines):
+    for i in xrange(1, numLines):
+        curLine = allLines[i].split(',')
+        pid = curLine[0]
+        uid = curLine[1]
+        pidArr[i-1] = pid
+        uidArr[i-1] = uid
+
+    np.savez_compressed("pid.npz", pidArr)
+    np.savez_compressed("uid.npz", uidArr)
+
+def groupByMovies(pids, uids):
     print "grouping movies..."
     movToUsers = {}
     for i in xrange(numLines-1):
-        curLine = allLines[i]
-        pid = curLine[0]
-        uid = curLine[1]
+        pid = pids[i]
+        uid = uids[i]
         if pid not in movToUsers:
             movToUsers[pid] = [uid]
         else:
@@ -52,13 +65,12 @@ def groupByMovies(allLines):
 
     return movToUsers
 
-def groupByUsers(allLines):
+def groupByUsers(pids, uids):
     print "grouping users..."
     userToMovies = {}
     for i in xrange(numLines-1):
-        curLine = allLines[i]
-        pid = curLine[0]
-        uid = curLine[1]
+        pid = pids[i]
+        uid = uids[i]
         if uid not in userToMovies:
             userToMovies[uid] = [pid]
         else:
@@ -89,20 +101,41 @@ def getPairDistances(movToUsers, userToMovs):
     avgFile.close()
 
     plt.hist(allAvgs, bins=20)
-    plt.savefig("faster1000Avgs.png")
+    plt.savefig("fastestAllAvgs.png")
 
 if __name__ == '__main__':
     startTime = time()
 
-    newAllLines = breakdownLines(allLinesCsv)
-    print len(newAllLines), ": time:", time() - startTime
+    if args.breakdown:
+        print "breaking down and saving uid and pid to npz's..."
+        breakdownLines(allLinesCsv)
+        t = time() - startTime
+        print "time to breakdown:", t
 
-    mtu = groupByMovies(newAllLines)
-    print len(mtu), ": time:", time() - startTime
+    print "reading npz files..."
+    npzFile = np.load("uid.npz", mmap_mode=None)
+    uids = npzFile[npzFile.files[0]]
+    print "UID SHAPE:", uids.shape
+    npzFile = np.load("pid.npz", mmap_mode=None)
+    pids = npzFile[npzFile.files[0]]
+    print "PID SHAPE:", pids.shape
+    t = time() - startTime
+    print "time to load uid and pid arrs:", t
+
+    #distributionOfMoviesUsers(pids, uids)
+    #t = time() - startTime
+    #print ": time dist of movie users:", t
+    #exit()
+
+    mtu = groupByMovies(pids, uids)
+    t = time() - startTime
+    print len(mtu), ": time:", t
     print "=============================="
-    utm = groupByUsers(newAllLines)
-    print len(utm), ": time:", time() - startTime
+    utm = groupByUsers(pids, uids)
+    t = time() - startTime
+    print len(utm), ": time:", t
 
     getPairDistances(mtu, utm)
-    print ": time:", time() - startTime
+    t = time() - startTime
+    print ": time:", t
     exit()
